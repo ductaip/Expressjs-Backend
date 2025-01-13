c87b60a6d0b97e356abdd7eb355e68711961d8ab
ductaip <dtai858@gmail.com>
2025-01-13 14:55:43 +0700

    feat: add routing for project

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express'
+import userRouter from './user.routes'
 const app = express()
-const port = 3000
+const port = 3002
 
-app.get('/', (req, res) => {
+app.post('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.use('/user', userRouter)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
