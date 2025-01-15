5cab1d2dcc86cd23815a9299d23ad2478109b709
ductaip <dtai858@gmail.com>
2025-01-15 22:30:37 +0700

    convert ds services to class, config dotenv for project

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import express from 'express'
 import usersRouter from './routes/users.routes' 
-import { run } from './services/database.services'
+import databaseService from './services/database.services'
 const app = express()
 const port = 3002
 
 app.use(express.json())
 app.use('/users', usersRouter)
 
-run().catch(console.dir)
+//connect to database
+databaseService.connect()
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- src/services/database.services.ts
+++ src/services/database.services.ts
@@ -1,15 +1,26 @@
 
+import { configDotenv } from 'dotenv';
 import { MongoClient, ServerApiVersion } from 'mongodb'
-const uri = "mongodb+srv://ductai:123456Aa.@threads.tv53y.mongodb.net/?retryWrites=true&w=majority&appName=threads";
+config()
 
-const client = new MongoClient(uri)
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@threads.tv53y.mongodb.net/?retryWrites=true&w=majority&appName=threads`;
+import {config} from 'dotenv'
 
-export async function run() {
-  try { 
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close()
+class DatabaseService {
+  client: MongoClient
+  constructor() {
+    this.client = new MongoClient(uri)
+  }
+  async connect() {
+    try { 
+      await this.client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await this.client.close()
+    }
   }
 }
+
+const databaseService = new DatabaseService()
+export default databaseService
