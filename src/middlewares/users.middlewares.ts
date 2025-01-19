dde6afeadeb5243062afc64430b8504dafc5e497
ductaip <dtai858@gmail.com>
2025-01-19 22:39:20 +0700

    feat: validate request body user with express-validator

--- src/middlewares/users.middlewares.ts
+++ src/middlewares/users.middlewares.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from 'express'
+import { checkSchema } from 'express-validator'
+import { validate } from '~/utils/validation'
 
 export const loginValidator = (req: Request, res: Response, next: NextFunction) => {
   const {email, password} = req.body
@@ -9,3 +11,40 @@ export const loginValidator = (req: Request, res: Response, next: NextFunction) 
     return
   } else next()
 }
+
+export const registerValidator = validate(checkSchema({
+  name: {
+    isString: true,
+    notEmpty: true,
+    trim: true,
+    isLength: {
+      options: {min: 4, max: 100}
+    }
+  },
+  email: {
+    isEmail: true,
+    notEmpty: true, 
+    trim: true
+  },
+  password: {
+    notEmpty: true,
+    isLength: {
+      options: { min: 4, max: 50 }
+    }
+  },
+  confirm_password: {
+    notEmpty: true,
+    isLength: {
+      options: { min: 4, max: 50 }
+    },
+    custom: {
+      options: (value, { req }) => {
+        if ( value !== req.body.password ) {
+          throw new Error('Password is not matched')
+        }
+        return true
+      }
+    }
+  }
+
+}))
