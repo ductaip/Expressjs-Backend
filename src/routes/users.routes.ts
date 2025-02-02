3efbe00e0f2c989ff32e7e9736931e80d1334d39
ductaip <dtai858@gmail.com>
2025-02-02 16:27:25 +0700

    config error handler and test

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -4,6 +4,15 @@ import { loginValidator, registerValidator } from '~/middlewares/users.middlewar
 const usersRouter = Router()
 
 usersRouter.post('/login', loginValidator, loginController)
-usersRouter.post('/register', registerValidator, registerController)
+usersRouter.post('/register', registerValidator, 
+  async (req, res, next) => {
+    console.log('error 1')
+    next(new Error('log Error from next'))
+  },
+  (err, req, res, next) => {
+    console.log('hi guys')
+    res.status(400).json({ error: err.message })
+  }
+)
 
 export default usersRouter
